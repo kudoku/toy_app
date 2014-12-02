0291bf730d0b2c3ac691b43e72f1d68106c05738
JPark <park.joonw@gmail.com>
2014-12-02 14:41:42 -0700

    hello world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "hello world!"
+    render text: "hello world! and toy app"
     # render text: "¡hola, mundo!"
   end
 end
